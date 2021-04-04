460245a3c5dcd84947e7b0558a952658c93cfdc4
cardiogram <felix@abi-ng.com>
2021-04-04 09:09:39 +0100

    initial commit

---
+++ Models/Prediction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ML.Data;
+
+namespace AnomalyDetection
+{
+    public class PredictionInput
+    {
+        [LoadColumn(0)]
+        public string X;
+
+        /// <summary>
+        /// Field to detect spikes on
+        /// </summary>
+        [LoadColumn(1)]
+        public float Y;
+    }
+
+    public class PredictionOutput
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(3)]
+        public double[] Prediction { get; set; }
+    }
+}
